387edde641ecf77b8fe53b4af8225a2d5624d0fb
Tihomir Jovic <jtihomir@gmail.com>
2019-03-08 12:59:37 +0100

    Add project files.

---
+++ Zadartak 8.2.1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadartak_8._2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite prirodan broj");
+            int broj = int.Parse(Console.ReadLine());
+
+
+            if (Static_Instanca.Prost(broj))
+            {
+                Console.Write("Broj je prost.");
+            }
+            else
+            {
+                Console.WriteLine("Broj nije prost.");
+            }
+            Static_Instanca SI = new Static_Instanca();
+            if (SI.Savrsen(broj))
+            {
+                Console.WriteLine("Broj je savrsen.");
+            }
+            else
+            {
+                Console.WriteLine("Broj nije savrsen.");
+            }
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
