9da3e4ae56019715a4ea40db2df982a0841cd086
Sherman259 <47825335+Sherman259@users.noreply.github.com>
2019-03-08 13:08:27 +0100

    Add files via upload

---
+++ Zadaca 8.1.3/Zadaca 8.1.3/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadaca_8._1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Novi objekt Zaposlenik
+            Zaposlenik Z1 = new Zaposlenik("Josip", "Josipovic", "8788593686856");
+
+            // Korisnik mora ispravno uneti broj bodova
+            bool provjeraUnosa = false;
+            while (!provjeraUnosa)
+            {
+                Console.Write("Unesite broj bodova");
+
+                try
+                {
+                    Z1.BrojBodova = double.Parse(Console.ReadLine());
+                    provjeraUnosa = true;
+                }
+
+                catch (FormatException ex) // specijalne greške
+                {
+                    Console.WriteLine("Greška:" + ex.Message);
+                }
+                catch (Exception ex)// ostale greške
+                {
+                    Console.WriteLine("Greška" + ex.Message);
+                }
+            }
+
+            provjeraUnosa = false;
+            while (!provjeraUnosa)
+            {
+                Console.Write("Unesite vrijednost boda:");
+                try
+                {
+                    Z1.VrijednostBoda = double.Parse(Console.ReadLine());
+                    provjeraUnosa = true;
+                }
+
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Greška" + ex.Message);
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Greška" + ex.Message);
+                }
+            }
+
+            // ispis
+            Console.WriteLine("Zaposlenik:" + Z1.Ime + "" + Z1.Prezime);
+            Console.WriteLine("Neto:" + Z1.NetoIzracunPlace());
+            Console.WriteLine("Porez:" + Z1.porez);
+            Console.WriteLine("Bruto" + Z1.BrutoIzracunPlace());
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+
+   
+    
+    
+}
---
+++ Zadaca 8.1.3/Zadaca 8.1.3/Zaposlenik.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadaca_8._1._3
+{
+    class Zaposlenik
+    {
+        #region Svojstva
+
+        string ime;
+        public string Ime
+        {
+            get { return ime; }
+            set { ime = value; }
+        }
+        string prezime;
+        public string Prezime
+        {
+            get { return prezime; }
+            set { prezime = value; }
+        }
+        string oib;
+        public string OIB
+        {
+
+            get { return oib; }
+            set { prezime = value; }
+        }
+
+        double brojBodova;
+        public double BrojBodova
+        {
+            get { return brojBodova; }
+            set
+            {
+                if (value > 0)
+                {
+                    brojBodova = value;
+                }
+
+                else
+                {
+                    Exception ex = new Exception("Mozete uneti samo pozitivan broj");
+                    throw ex;
+                }
+            }
+        }
+
+        double vrijednostBoda;
+        public double VrijednostBoda
+        {
+            get { return vrijednostBoda; }
+            set
+
+            {
+                if (value > 0)
+                {
+                    vrijednostBoda = value;
+                }
+                else
+                {
+                    Exception ex = new Exception("Mozete uneti samo pozitivan broj");
+                    throw ex;
+                }
+            }
+        }
+
+        // ReadOnly svojstvo
+        public double porez
+        {
+            get
+            {
+                double neto = this.NetoIzracunPlace();
+                if (neto < 3000)
+                {
+                    return neto * 0.06;
+                }
+
+                else if (neto < 6000)
+                {
+                    return neto * 0.12;
+                }
+                else
+                {
+                    return neto * 0.2;
+                }
+            }
+        }
+
+
+
+        #endregion
+
+        #region  metode
+        public double NetoIzracunPlace()
+        {
+            return this.BrojBodova * this.VrijednostBoda;
+        }
+        public double BrutoIzracunPlace()
+        {
+            return this.NetoIzracunPlace() + this.porez;
+        }
+
+        #endregion
+
+        #region Konstruktori
+
+        public Zaposlenik() { }
+
+        public Zaposlenik(string ime, string prezime)
+        {
+
+            this.Ime = ime;
+            this.Prezime = prezime;
+        }
+
+        public Zaposlenik(string ime, string prezime, string oib)
+        {
+
+            this.Ime = ime;
+            this.Prezime = prezime;
+            this.oib = OIB;
+        }
+
+        #endregion
+    }
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
